917bdf74e0f654c50e9c5cc7b40bfd0b4fdb9d6a
Yulia Tribunskaya <ytribunskaya@kameleoon.com>
2021-09-18 18:04:30 +0300

    did axios get request to test url

--- src/Task2.jsx
+++ src/Task2.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { AddPizzeriaButton } from "./AddPizzeriaButton";
 import { PizzeriasCount } from "./PizzeriasCount";
+import axios from 'axios';
 
 export const Task2 = () => {
     const [countPizza, setCountPizza] = useState();
@@ -14,6 +15,15 @@ export const Task2 = () => {
         }
         fetchData();
     }, []);
+
+    let [persons, getPersons] = useState();
+    useEffect(() => {
+        axios.get(`https://jsonplaceholder.typicode.com/users`)
+            .then(res => {
+                getPersons(res.data);
+                return persons;
+            })
+    });
     return (
         <>
             <h1>Задача 2</h1>
@@ -48,6 +58,10 @@ export const Task2 = () => {
             <hr />
             <PizzeriasCount countPizza={countPizza} />
             <AddPizzeriaButton setCountPizza={setCountPizza} countPizza={countPizza} />
+            <ul>
+                GET запрос:
+                {persons && persons.map(person => <li>{person.name}</li>)}
+            </ul>
         </>
     );
 };
